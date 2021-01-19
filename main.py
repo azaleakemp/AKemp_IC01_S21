95a16a32a3be42c2514b83a8fa1476fa7970dbfe
azaleakemp <kempazalea@gmail.com>
2021-01-19 16:59:32 -0500

    adding proper files. had to fix directory issue on machine

---
+++ main.py
@@ -0,0 +1,16 @@
+"""*******************************************************
+CS 104: Introduction to Programming
+Description: sample hello script
+Date: 1/14/2021
+Author(s): Azalea Kemp
+*******************************************************"""
+
+print('Hello World')
+
+name = input('Please input your name: ')
+for i in range(15):
+    print(name + " is the best")
+
+dog = input('Please input the name of your dog: ')
+for i in range(1):
+    print(dog + ' is the name of ' + name + '\'s dog.')
